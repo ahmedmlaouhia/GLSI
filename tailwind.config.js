81141e7f33f48fd2255a0bee23e072bad10d6e26
ahmedmlaouhia <ahmedmlaouhia6@gmail.com>
2024-05-25 04:23:36 +0100

    updated things up

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,7 @@ module.exports = {
     extend: {
       boxShadow: {
         custom: "0 0px 25px 5px rgba(99, 83, 226, 0.8)",
+        custom2: "0 0px 10px 2px rgba(255,255,255, 0.5)",
       },
     },
   },
